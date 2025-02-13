6bc6b51863ba04a89efda8859eec198f13f64b81
noobOriginals <noobOriginals@xx.com>
2025-02-13 11:54:16 +0200

    Fixed window class

--- src/Header Files/api/window.hpp
+++ src/Header Files/api/window.hpp
@@ -21,9 +21,8 @@ namespace window {
 }
 class Window {
 public:
-	Window() { address = 0; width = 0; height = 0; title = ""; fscreen = false; }
-	Window(Window& w) = delete;
 	Window(const uint32 width, const uint32 height, const std::string title);
+	Window(Window&) = delete;
 	~Window();
 	// Utility
 	void close();
